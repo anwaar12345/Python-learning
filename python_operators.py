45ca688f5aa95bad4c9d250952b6f327676f161a
anwaar12345 <syedanwar016@gmail.com>
2022-07-23 20:27:29 +0500

    operators

---
+++ python_operators.py
@@ -0,0 +1,19 @@
+from pprint import pprint
+
+
+print("the sum of 3 + 2 = ", 3 + 2)
+print("the sum of 3 - 2 = ", 3 - 2)
+print("the sum of 3 / 3 = ", 3 / 3) # division will always return float  1.0
+print("the sum of 3 * 2 = ", 3 * 2) 
+
+age = 27
+age +=  1 #equal to age = age + 2
+age -= 1
+age *= age 
+age /= 27
+age = 1
+print("older") if age > 0 and age == 27 else print("younger") #ternary
+
+print(age > 30)
+print("age: ",age)
+print(age > 2 or age ==0)
