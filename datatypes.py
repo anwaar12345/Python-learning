c434d7af7fb7080f73068491826d980769acbd14
anwaar12345 <syedanwar016@gmail.com>
2022-07-23 14:25:13 +0500

    data types

---
+++ datatypes.py
@@ -0,0 +1,32 @@
+from datetime import date
+
+
+name  = "Syed Anwar Ahmed Shah is learning new skill\"'s"
+
+name_with_out_escapes = '''python is "awesome"  and 'easy to learn' language '''
+
+intro = "%s and %s "%(name,name_with_out_escapes)
+
+new_intro = f"{name} and {name_with_out_escapes}"
+
+print(f'''{intro} {new_intro}''')
+
+age  = date.today().year - date(1995,3,25).year
+
+print(f'''{name} 
+
+and age is {age}''')
+
+name_check = False
+
+if(age == 27):
+     name_check = True
+     print(name_check)
+if(age != 1):
+    d=None
+    print(d)
+
+height = 5.4
+
+print(f"heignt {height} ")
+print(type(height),type(age),type(name_check),type(d))
