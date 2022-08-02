6118e60808132feb2fd65681d836909de7a21d9e
anwaar12345 <syedanwar016@gmail.com>
2022-08-03 02:19:47 +0500

    done till tupples

--- Syntax_Basic/tuples_count.py
+++ Syntax_Basic/tuples_count.py
@@ -2,7 +2,7 @@ from itertools import count
 import os
 clear = lambda : os.system('clear')
 clear()
-tupples = (1,2,2,3,0,1,0,0)
+tupples = (1,2,2,3,0,1,0,0,"s",True)
 
 print("count zeroes: ",tupples.count(0))
 
