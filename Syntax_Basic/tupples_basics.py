226472240a089700e3c2252c2d2156555ca627e9
anwaar12345 <syedanwar016@gmail.com>
2022-08-03 01:23:32 +0500

    done till tupples

---
+++ Syntax_Basic/tupples_basics.py
@@ -0,0 +1,15 @@
+#tupples are immutable / constants
+tupples = ()
+print(tupples) #empty tupples
+
+tupples = (3,) #tupple with single element
+
+print(tupples) #
+
+tupples = (1,2,3,4)
+
+# tupples[0] = 2 #tupples are immutable
+
+
+
+print(tupples[0:]) #same like lists and string slicing
---
+++ tupples_functions.py
@@ -0,0 +1,9 @@
+tupples = (1,2,3,3,"1",True) #can have different types like lists
+
+print(tupples[len(tupples)-1])
+
+#tupple method
+
+print(tupples.count(2))
+
+print(tupples.index(3))
